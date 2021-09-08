23f24acfa172af753a40bbdbccd9ebc4da0e90e9
Shubham Chaudhary <61886393+Stellar-X@users.noreply.github.com>
2021-09-08 22:51:50 +0530

    Initialize version2

---
+++ app/src/main/java/com/example/blood_bank_app/LifeSavers/Activities/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.blood_bank_app.LifeSavers.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.blood_bank_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.button_become_donor);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}

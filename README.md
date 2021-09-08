377ffbae2be9b64aeaa9e95856bef499b14bab20
Shubham Chaudhary <61886393+Stellar-X@users.noreply.github.com>
2021-09-08 22:51:49 +0530

    Initialize version-5(yash)

---
+++ README.md
@@ -0,0 +1,6 @@
+# Blood_Bank_App
+Add all the progress/changes made in the project here (in the format as below).
+
+Shubham [19-02-21 09:14pm] : Initial Commit (Main Project Started)
+
+Shubham [22-02-21 10:25pm] : Added Registration Page Activity and functionalities to Login Page (Main Activity)
---
+++ app/src/main/java/com/example/blood_bank_app/LifeSavers/Utils/EndPoints.java
@@ -0,0 +1,7 @@
+package com.example.blood_bank_app.LifeSavers.Utils;
+
+public class EndPoints {
+    private static final String base_url = "https://sassy-pbl.000webhostapp.com/";
+    public static final String register_url = base_url + "register.php";
+    public static final String login_url = base_url + "login.php";
+}
